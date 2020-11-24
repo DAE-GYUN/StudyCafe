46c10de5148eb31e4ffbe8821f9d6ef5d1088fe0
DAE-GYUN <f45ee@naver.com>
2020-11-24 15:42:34 +0900

    LockersForm ADD

---
+++ WindowsFormsApp1/LockersForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class LockersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLockersCancel = new System.Windows.Forms.Button();
+            this.btnLocker1 = new System.Windows.Forms.Button();
+            this.btnLocker2 = new System.Windows.Forms.Button();
+            this.btnLocker3 = new System.Windows.Forms.Button();
+            this.btnLocker6 = new System.Windows.Forms.Button();
+            this.btnLocker5 = new System.Windows.Forms.Button();
+            this.btnLocker4 = new System.Windows.Forms.Button();
+            this.btnLocker7 = new System.Windows.Forms.Button();
+            this.btnLocker8 = new System.Windows.Forms.Button();
+            this.btnLocker9 = new System.Windows.Forms.Button();
+            this.btnLocker12 = new System.Windows.Forms.Button();
+            this.btnLocker11 = new System.Windows.Forms.Button();
+            this.btnLocker10 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker12, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker11, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker10, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocker8, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(206, 67);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(481, 296);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnLockersCancel
+            // 
+            this.btnLockersCancel.Location = new System.Drawing.Point(612, 395);
+            this.btnLockersCancel.Name = "btnLockersCancel";
+            this.btnLockersCancel.Size = new System.Drawing.Size(75, 43);
+            this.btnLockersCancel.TabIndex = 1;
+            this.btnLockersCancel.Text = "취소";
+            this.btnLockersCancel.UseVisualStyleBackColor = true;
+            this.btnLockersCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLocker1
+            // 
+            this.btnLocker1.Location = new System.Drawing.Point(3, 3);
+            this.btnLocker1.Name = "btnLocker1";
+            this.btnLocker1.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker1.TabIndex = 0;
+            this.btnLocker1.Text = "1번";
+            this.btnLocker1.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker2
+            // 
+            this.btnLocker2.Location = new System.Drawing.Point(163, 3);
+            this.btnLocker2.Name = "btnLocker2";
+            this.btnLocker2.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker2.TabIndex = 0;
+            this.btnLocker2.Text = "2번";
+            this.btnLocker2.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker3
+            // 
+            this.btnLocker3.Location = new System.Drawing.Point(323, 3);
+            this.btnLocker3.Name = "btnLocker3";
+            this.btnLocker3.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker3.TabIndex = 0;
+            this.btnLocker3.Text = "3번";
+            this.btnLocker3.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker6
+            // 
+            this.btnLocker6.Location = new System.Drawing.Point(323, 77);
+            this.btnLocker6.Name = "btnLocker6";
+            this.btnLocker6.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker6.TabIndex = 0;
+            this.btnLocker6.Text = "6번";
+            this.btnLocker6.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker5
+            // 
+            this.btnLocker5.Location = new System.Drawing.Point(163, 77);
+            this.btnLocker5.Name = "btnLocker5";
+            this.btnLocker5.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker5.TabIndex = 0;
+            this.btnLocker5.Text = "5번";
+            this.btnLocker5.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker4
+            // 
+            this.btnLocker4.Location = new System.Drawing.Point(3, 77);
+            this.btnLocker4.Name = "btnLocker4";
+            this.btnLocker4.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker4.TabIndex = 0;
+            this.btnLocker4.Text = "4번";
+            this.btnLocker4.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker7
+            // 
+            this.btnLocker7.Location = new System.Drawing.Point(3, 151);
+            this.btnLocker7.Name = "btnLocker7";
+            this.btnLocker7.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker7.TabIndex = 0;
+            this.btnLocker7.Text = "7번";
+            this.btnLocker7.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker8
+            // 
+            this.btnLocker8.Location = new System.Drawing.Point(163, 151);
+            this.btnLocker8.Name = "btnLocker8";
+            this.btnLocker8.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker8.TabIndex = 0;
+            this.btnLocker8.Text = "8번";
+            this.btnLocker8.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker9
+            // 
+            this.btnLocker9.Location = new System.Drawing.Point(323, 151);
+            this.btnLocker9.Name = "btnLocker9";
+            this.btnLocker9.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker9.TabIndex = 0;
+            this.btnLocker9.Text = "9번";
+            this.btnLocker9.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker12
+            // 
+            this.btnLocker12.Location = new System.Drawing.Point(323, 225);
+            this.btnLocker12.Name = "btnLocker12";
+            this.btnLocker12.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker12.TabIndex = 0;
+            this.btnLocker12.Text = "12번";
+            this.btnLocker12.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker11
+            // 
+            this.btnLocker11.Location = new System.Drawing.Point(163, 225);
+            this.btnLocker11.Name = "btnLocker11";
+            this.btnLocker11.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker11.TabIndex = 0;
+            this.btnLocker11.Text = "11번";
+            this.btnLocker11.UseVisualStyleBackColor = true;
+            // 
+            // btnLocker10
+            // 
+            this.btnLocker10.Location = new System.Drawing.Point(3, 225);
+            this.btnLocker10.Name = "btnLocker10";
+            this.btnLocker10.Size = new System.Drawing.Size(75, 23);
+            this.btnLocker10.TabIndex = 0;
+            this.btnLocker10.Text = "10번";
+            this.btnLocker10.UseVisualStyleBackColor = true;
+            // 
+            // LockersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLockersCancel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LockersForm";
+            this.Text = "LockersForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnLockersCancel;
+        private System.Windows.Forms.Button btnLocker1;
+        private System.Windows.Forms.Button btnLocker2;
+        private System.Windows.Forms.Button btnLocker3;
+        private System.Windows.Forms.Button btnLocker6;
+        private System.Windows.Forms.Button btnLocker5;
+        private System.Windows.Forms.Button btnLocker4;
+        private System.Windows.Forms.Button btnLocker7;
+        private System.Windows.Forms.Button btnLocker9;
+        private System.Windows.Forms.Button btnLocker12;
+        private System.Windows.Forms.Button btnLocker11;
+        private System.Windows.Forms.Button btnLocker10;
+        private System.Windows.Forms.Button btnLocker8;
+    }
+}
