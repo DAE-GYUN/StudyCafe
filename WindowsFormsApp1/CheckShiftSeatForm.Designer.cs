044a308c74c7c2d64c3a63594a0e4546e17a4c40
leeseonghyuck <zzangjb2@gmail.com>
2020-11-24 17:27:26 +0900

    좌석Form

--- WindowsFormsApp1/CheckShiftSeatForm.Designer.cs
+++ WindowsFormsApp1/CheckShiftSeatForm.Designer.cs
@@ -29,12 +29,265 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSeatNumber1 = new System.Windows.Forms.Button();
+            this.btnSeatNumber2 = new System.Windows.Forms.Button();
+            this.btnSeatNumber3 = new System.Windows.Forms.Button();
+            this.btnSeatNumber4 = new System.Windows.Forms.Button();
+            this.btnSeatNumber5 = new System.Windows.Forms.Button();
+            this.btnSeatNumber6 = new System.Windows.Forms.Button();
+            this.btnSeatNumber7 = new System.Windows.Forms.Button();
+            this.btnSeatNumber8 = new System.Windows.Forms.Button();
+            this.btnSeatNumber9 = new System.Windows.Forms.Button();
+            this.btnSeatNumber10 = new System.Windows.Forms.Button();
+            this.btnSeatNumber11 = new System.Windows.Forms.Button();
+            this.btnSeatNumber12 = new System.Windows.Forms.Button();
+            this.btnSeatNumber13 = new System.Windows.Forms.Button();
+            this.btnSeatNumber14 = new System.Windows.Forms.Button();
+            this.btnSeatNumber15 = new System.Windows.Forms.Button();
+            this.btnRoomNumber1 = new System.Windows.Forms.Button();
+            this.btnRoomNumber2 = new System.Windows.Forms.Button();
+            this.btnRoomNumber3 = new System.Windows.Forms.Button();
+            this.btnRoomNumber4 = new System.Windows.Forms.Button();
+            this.btnSeatNumber16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSeatNumber1
+            // 
+            this.btnSeatNumber1.Location = new System.Drawing.Point(27, 27);
+            this.btnSeatNumber1.Name = "btnSeatNumber1";
+            this.btnSeatNumber1.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber1.TabIndex = 0;
+            this.btnSeatNumber1.Text = "1";
+            this.btnSeatNumber1.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber2
+            // 
+            this.btnSeatNumber2.Location = new System.Drawing.Point(27, 87);
+            this.btnSeatNumber2.Name = "btnSeatNumber2";
+            this.btnSeatNumber2.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber2.TabIndex = 0;
+            this.btnSeatNumber2.Text = "2";
+            this.btnSeatNumber2.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber3
+            // 
+            this.btnSeatNumber3.Location = new System.Drawing.Point(27, 156);
+            this.btnSeatNumber3.Name = "btnSeatNumber3";
+            this.btnSeatNumber3.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber3.TabIndex = 0;
+            this.btnSeatNumber3.Text = "3";
+            this.btnSeatNumber3.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber4
+            // 
+            this.btnSeatNumber4.Location = new System.Drawing.Point(27, 226);
+            this.btnSeatNumber4.Name = "btnSeatNumber4";
+            this.btnSeatNumber4.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber4.TabIndex = 0;
+            this.btnSeatNumber4.Text = "4";
+            this.btnSeatNumber4.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber5
+            // 
+            this.btnSeatNumber5.Location = new System.Drawing.Point(27, 296);
+            this.btnSeatNumber5.Name = "btnSeatNumber5";
+            this.btnSeatNumber5.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber5.TabIndex = 0;
+            this.btnSeatNumber5.Text = "5";
+            this.btnSeatNumber5.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber6
+            // 
+            this.btnSeatNumber6.Location = new System.Drawing.Point(27, 372);
+            this.btnSeatNumber6.Name = "btnSeatNumber6";
+            this.btnSeatNumber6.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber6.TabIndex = 0;
+            this.btnSeatNumber6.Text = "6";
+            this.btnSeatNumber6.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber7
+            // 
+            this.btnSeatNumber7.Location = new System.Drawing.Point(27, 452);
+            this.btnSeatNumber7.Name = "btnSeatNumber7";
+            this.btnSeatNumber7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSeatNumber7.Size = new System.Drawing.Size(86, 39);
+            this.btnSeatNumber7.TabIndex = 1;
+            this.btnSeatNumber7.Text = "7";
+            this.btnSeatNumber7.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber8
+            // 
+            this.btnSeatNumber8.Location = new System.Drawing.Point(140, 452);
+            this.btnSeatNumber8.Name = "btnSeatNumber8";
+            this.btnSeatNumber8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSeatNumber8.Size = new System.Drawing.Size(86, 39);
+            this.btnSeatNumber8.TabIndex = 1;
+            this.btnSeatNumber8.Text = "8";
+            this.btnSeatNumber8.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber9
+            // 
+            this.btnSeatNumber9.Location = new System.Drawing.Point(257, 452);
+            this.btnSeatNumber9.Name = "btnSeatNumber9";
+            this.btnSeatNumber9.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSeatNumber9.Size = new System.Drawing.Size(86, 39);
+            this.btnSeatNumber9.TabIndex = 1;
+            this.btnSeatNumber9.Text = "9";
+            this.btnSeatNumber9.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber10
+            // 
+            this.btnSeatNumber10.Location = new System.Drawing.Point(370, 452);
+            this.btnSeatNumber10.Name = "btnSeatNumber10";
+            this.btnSeatNumber10.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSeatNumber10.Size = new System.Drawing.Size(86, 39);
+            this.btnSeatNumber10.TabIndex = 1;
+            this.btnSeatNumber10.Text = "10";
+            this.btnSeatNumber10.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber11
+            // 
+            this.btnSeatNumber11.Location = new System.Drawing.Point(481, 452);
+            this.btnSeatNumber11.Name = "btnSeatNumber11";
+            this.btnSeatNumber11.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSeatNumber11.Size = new System.Drawing.Size(86, 39);
+            this.btnSeatNumber11.TabIndex = 1;
+            this.btnSeatNumber11.Text = "11";
+            this.btnSeatNumber11.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber12
+            // 
+            this.btnSeatNumber12.Location = new System.Drawing.Point(298, 27);
+            this.btnSeatNumber12.Name = "btnSeatNumber12";
+            this.btnSeatNumber12.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber12.TabIndex = 0;
+            this.btnSeatNumber12.Text = "12";
+            this.btnSeatNumber12.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber13
+            // 
+            this.btnSeatNumber13.Location = new System.Drawing.Point(298, 87);
+            this.btnSeatNumber13.Name = "btnSeatNumber13";
+            this.btnSeatNumber13.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber13.TabIndex = 0;
+            this.btnSeatNumber13.Text = "13";
+            this.btnSeatNumber13.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber14
+            // 
+            this.btnSeatNumber14.Location = new System.Drawing.Point(298, 156);
+            this.btnSeatNumber14.Name = "btnSeatNumber14";
+            this.btnSeatNumber14.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber14.TabIndex = 0;
+            this.btnSeatNumber14.Text = "14";
+            this.btnSeatNumber14.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber15
+            // 
+            this.btnSeatNumber15.Location = new System.Drawing.Point(298, 226);
+            this.btnSeatNumber15.Name = "btnSeatNumber15";
+            this.btnSeatNumber15.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber15.TabIndex = 0;
+            this.btnSeatNumber15.Text = "15";
+            this.btnSeatNumber15.UseVisualStyleBackColor = true;
+            // 
+            // btnRoomNumber1
+            // 
+            this.btnRoomNumber1.Location = new System.Drawing.Point(763, 30);
+            this.btnRoomNumber1.Name = "btnRoomNumber1";
+            this.btnRoomNumber1.Size = new System.Drawing.Size(252, 97);
+            this.btnRoomNumber1.TabIndex = 0;
+            this.btnRoomNumber1.Text = "스터디룸1(4인)";
+            this.btnRoomNumber1.UseVisualStyleBackColor = true;
+            // 
+            // btnRoomNumber2
+            // 
+            this.btnRoomNumber2.Location = new System.Drawing.Point(763, 133);
+            this.btnRoomNumber2.Name = "btnRoomNumber2";
+            this.btnRoomNumber2.Size = new System.Drawing.Size(252, 97);
+            this.btnRoomNumber2.TabIndex = 0;
+            this.btnRoomNumber2.Text = "스터디룸2(4인)";
+            this.btnRoomNumber2.UseVisualStyleBackColor = true;
+            // 
+            // btnRoomNumber3
+            // 
+            this.btnRoomNumber3.Location = new System.Drawing.Point(763, 237);
+            this.btnRoomNumber3.Name = "btnRoomNumber3";
+            this.btnRoomNumber3.Size = new System.Drawing.Size(252, 97);
+            this.btnRoomNumber3.TabIndex = 0;
+            this.btnRoomNumber3.Text = "스터디룸3(6인)";
+            this.btnRoomNumber3.UseVisualStyleBackColor = true;
+            // 
+            // btnRoomNumber4
+            // 
+            this.btnRoomNumber4.Location = new System.Drawing.Point(763, 343);
+            this.btnRoomNumber4.Name = "btnRoomNumber4";
+            this.btnRoomNumber4.Size = new System.Drawing.Size(252, 97);
+            this.btnRoomNumber4.TabIndex = 0;
+            this.btnRoomNumber4.Text = "스터디룸4(6인)";
+            this.btnRoomNumber4.UseVisualStyleBackColor = true;
+            // 
+            // btnSeatNumber16
+            // 
+            this.btnSeatNumber16.Location = new System.Drawing.Point(298, 296);
+            this.btnSeatNumber16.Name = "btnSeatNumber16";
+            this.btnSeatNumber16.Size = new System.Drawing.Size(86, 38);
+            this.btnSeatNumber16.TabIndex = 0;
+            this.btnSeatNumber16.Text = "16";
+            this.btnSeatNumber16.UseVisualStyleBackColor = true;
+            // 
+            // CheckShiftSeatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1037, 534);
+            this.Controls.Add(this.btnSeatNumber11);
+            this.Controls.Add(this.btnSeatNumber10);
+            this.Controls.Add(this.btnSeatNumber9);
+            this.Controls.Add(this.btnSeatNumber8);
+            this.Controls.Add(this.btnSeatNumber7);
+            this.Controls.Add(this.btnSeatNumber6);
+            this.Controls.Add(this.btnSeatNumber5);
+            this.Controls.Add(this.btnRoomNumber4);
+            this.Controls.Add(this.btnSeatNumber16);
+            this.Controls.Add(this.btnSeatNumber15);
+            this.Controls.Add(this.btnSeatNumber4);
+            this.Controls.Add(this.btnRoomNumber3);
+            this.Controls.Add(this.btnSeatNumber14);
+            this.Controls.Add(this.btnSeatNumber3);
+            this.Controls.Add(this.btnRoomNumber2);
+            this.Controls.Add(this.btnSeatNumber13);
+            this.Controls.Add(this.btnSeatNumber2);
+            this.Controls.Add(this.btnRoomNumber1);
+            this.Controls.Add(this.btnSeatNumber12);
+            this.Controls.Add(this.btnSeatNumber1);
+            this.Name = "CheckShiftSeatForm";
             this.Text = "CheckShiftSeatForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSeatNumber1;
+        private System.Windows.Forms.Button btnSeatNumber2;
+        private System.Windows.Forms.Button btnSeatNumber3;
+        private System.Windows.Forms.Button btnSeatNumber4;
+        private System.Windows.Forms.Button btnSeatNumber5;
+        private System.Windows.Forms.Button btnSeatNumber6;
+        private System.Windows.Forms.Button btnSeatNumber7;
+        private System.Windows.Forms.Button btnSeatNumber8;
+        private System.Windows.Forms.Button btnSeatNumber9;
+        private System.Windows.Forms.Button btnSeatNumber10;
+        private System.Windows.Forms.Button btnSeatNumber11;
+        private System.Windows.Forms.Button btnSeatNumber12;
+        private System.Windows.Forms.Button btnSeatNumber13;
+        private System.Windows.Forms.Button btnSeatNumber14;
+        private System.Windows.Forms.Button btnSeatNumber15;
+        private System.Windows.Forms.Button btnRoomNumber1;
+        private System.Windows.Forms.Button btnRoomNumber2;
+        private System.Windows.Forms.Button btnRoomNumber3;
+        private System.Windows.Forms.Button btnRoomNumber4;
+        private System.Windows.Forms.Button btnSeatNumber16;
     }
 }
