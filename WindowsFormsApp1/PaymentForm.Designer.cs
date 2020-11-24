2a7567351818a459394066a00965790f893b6ede
Kang Dong Jin <ghal789@naver.com>
2020-11-24 17:21:31 +0900

    Patment_Base

--- WindowsFormsApp1/PaymentForm.Designer.cs
+++ WindowsFormsApp1/PaymentForm.Designer.cs
@@ -29,12 +29,112 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txbUserNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbTotalPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.txbCart = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbUserNumber
+            // 
+            this.txbUserNumber.Location = new System.Drawing.Point(279, 29);
+            this.txbUserNumber.Name = "txbUserNumber";
+            this.txbUserNumber.Size = new System.Drawing.Size(198, 25);
+            this.txbUserNumber.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "회원번호";
+            // 
+            // txbTotalPrice
+            // 
+            this.txbTotalPrice.Location = new System.Drawing.Point(279, 211);
+            this.txbTotalPrice.Name = "txbTotalPrice";
+            this.txbTotalPrice.Size = new System.Drawing.Size(198, 25);
+            this.txbTotalPrice.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "총가격";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(213, 273);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(107, 43);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "돌아가기";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.Location = new System.Drawing.Point(373, 273);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(107, 43);
+            this.btnPurchase.TabIndex = 2;
+            this.btnPurchase.Text = "결제";
+            this.btnPurchase.UseVisualStyleBackColor = true;
+            // 
+            // txbCart
+            // 
+            this.txbCart.Location = new System.Drawing.Point(279, 80);
+            this.txbCart.Multiline = true;
+            this.txbCart.Name = "txbCart";
+            this.txbCart.Size = new System.Drawing.Size(198, 79);
+            this.txbCart.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "선택항목";
+            // 
+            // PaymentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbCart);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbTotalPrice);
+            this.Controls.Add(this.txbUserNumber);
+            this.Name = "PaymentForm";
             this.Text = "PaymentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txbUserNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbTotalPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.TextBox txbCart;
+        private System.Windows.Forms.Label label3;
     }
 }
