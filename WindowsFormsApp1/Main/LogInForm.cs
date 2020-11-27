3ed27cb4526ba3f99d7639b0f9c0b7c3e084991b
Lee-minkyun <73146493+Lee-minkyun@users.noreply.github.com>
2020-11-27 20:08:54 +0900

    User Ui

--- WindowsFormsApp1/Main/LogInForm.cs
+++ WindowsFormsApp1/Main/LogInForm.cs
@@ -60,5 +60,10 @@ namespace WindowsFormsApp1
             }
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WindowsFormsApp1/Main/MainForm.cs
+++ WindowsFormsApp1/Main/MainForm.cs
@@ -70,5 +70,10 @@ namespace WindowsFormsApp1
             PurchaseForm puchaseForm = new PurchaseForm();
             puchaseForm.ShowDialog();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
