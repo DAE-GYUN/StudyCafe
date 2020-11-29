320181cc9285018039d61064cece346de1cbc3c1
Lee-minkyun <73146493+Lee-minkyun@users.noreply.github.com>
2020-11-29 14:53:52 +0900

    UI PurchaseForm

--- WindowsFormsApp1/Main/PurchaseForm.Designer.cs
+++ WindowsFormsApp1/Main/PurchaseForm.Designer.cs
@@ -29,25 +29,34 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PurchaseForm));
             this.btnSeetTimeCharnging = new System.Windows.Forms.Button();
             this.btnStudyRoom = new System.Windows.Forms.Button();
             this.btnLockers = new System.Windows.Forms.Button();
             this.btnMain = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.paymentControl1 = new WindowsFormsApp1.PaymentControl();
-            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSeetTimeCharnging
             // 
             this.btnSeetTimeCharnging.BackColor = System.Drawing.Color.Transparent;
+            this.btnSeetTimeCharnging.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSeetTimeCharnging.BackgroundImage")));
+            this.btnSeetTimeCharnging.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSeetTimeCharnging.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnSeetTimeCharnging.FlatAppearance.BorderSize = 0;
             this.btnSeetTimeCharnging.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSeetTimeCharnging.Location = new System.Drawing.Point(79, 107);
+            this.btnSeetTimeCharnging.Location = new System.Drawing.Point(3, 82);
+            this.btnSeetTimeCharnging.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSeetTimeCharnging.Name = "btnSeetTimeCharnging";
-            this.btnSeetTimeCharnging.Size = new System.Drawing.Size(160, 72);
+            this.btnSeetTimeCharnging.Size = new System.Drawing.Size(167, 64);
             this.btnSeetTimeCharnging.TabIndex = 1;
             this.btnSeetTimeCharnging.Text = "시간설정";
             this.btnSeetTimeCharnging.UseVisualStyleBackColor = false;
@@ -56,11 +65,15 @@ namespace WindowsFormsApp1
             // btnStudyRoom
             // 
             this.btnStudyRoom.BackColor = System.Drawing.Color.Transparent;
+            this.btnStudyRoom.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStudyRoom.BackgroundImage")));
+            this.btnStudyRoom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStudyRoom.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnStudyRoom.FlatAppearance.BorderSize = 0;
             this.btnStudyRoom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStudyRoom.Location = new System.Drawing.Point(297, 107);
+            this.btnStudyRoom.Location = new System.Drawing.Point(176, 82);
+            this.btnStudyRoom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnStudyRoom.Name = "btnStudyRoom";
-            this.btnStudyRoom.Size = new System.Drawing.Size(161, 72);
+            this.btnStudyRoom.Size = new System.Drawing.Size(167, 64);
             this.btnStudyRoom.TabIndex = 1;
             this.btnStudyRoom.Text = "스터디룸";
             this.btnStudyRoom.UseVisualStyleBackColor = false;
@@ -69,11 +82,15 @@ namespace WindowsFormsApp1
             // btnLockers
             // 
             this.btnLockers.BackColor = System.Drawing.Color.Transparent;
+            this.btnLockers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLockers.BackgroundImage")));
+            this.btnLockers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLockers.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnLockers.FlatAppearance.BorderSize = 0;
             this.btnLockers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLockers.Location = new System.Drawing.Point(511, 107);
+            this.btnLockers.Location = new System.Drawing.Point(349, 82);
+            this.btnLockers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLockers.Name = "btnLockers";
-            this.btnLockers.Size = new System.Drawing.Size(163, 72);
+            this.btnLockers.Size = new System.Drawing.Size(167, 64);
             this.btnLockers.TabIndex = 1;
             this.btnLockers.Text = "사물함";
             this.btnLockers.UseVisualStyleBackColor = false;
@@ -82,65 +99,111 @@ namespace WindowsFormsApp1
             // btnMain
             // 
             this.btnMain.BackColor = System.Drawing.Color.Transparent;
+            this.btnMain.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMain.BackgroundImage")));
+            this.btnMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnMain.FlatAppearance.BorderSize = 0;
             this.btnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMain.Location = new System.Drawing.Point(533, 235);
+            this.btnMain.Location = new System.Drawing.Point(522, 82);
+            this.btnMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnMain.Name = "btnMain";
-            this.btnMain.Size = new System.Drawing.Size(163, 71);
+            this.btnMain.Size = new System.Drawing.Size(169, 64);
             this.btnMain.TabIndex = 1;
             this.btnMain.Text = "메인화면";
             this.btnMain.UseVisualStyleBackColor = false;
             this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.btnSeetTimeCharnging);
-            this.groupBox1.Controls.Add(this.btnMain);
-            this.groupBox1.Controls.Add(this.btnStudyRoom);
-            this.groupBox1.Controls.Add(this.btnLockers);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(794, 312);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.paymentControl1, 0, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.paymentControl1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 132F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.77778F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.94444F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 360);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // paymentControl1
             // 
-            this.paymentControl1.Location = new System.Drawing.Point(3, 321);
+            this.paymentControl1.Location = new System.Drawing.Point(3, 271);
+            this.paymentControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.paymentControl1.Name = "paymentControl1";
-            this.paymentControl1.Size = new System.Drawing.Size(794, 126);
+            this.paymentControl1.Size = new System.Drawing.Size(694, 87);
             this.paymentControl1.TabIndex = 3;
             this.paymentControl1.Load += new System.EventHandler(this.paymentControl1_Load);
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.btnMain, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnSeetTimeCharnging, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnLockers, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnStudyRoom, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 75);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(694, 148);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(694, 66);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Title;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // PurchaseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(700, 360);
             this.Controls.Add(this.tableLayoutPanel1);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PurchaseForm";
             this.Text = "PurchaseForm";
-            this.groupBox1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -150,8 +213,10 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button btnStudyRoom;
         private System.Windows.Forms.Button btnLockers;
         private System.Windows.Forms.Button btnMain;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private PaymentControl paymentControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
