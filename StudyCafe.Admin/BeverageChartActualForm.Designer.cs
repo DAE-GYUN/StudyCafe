a74a791250495660c89abb23b4dd2e3ca2c3f4bc
leeseonghyuck <zzangjb2@gmail.com>
2020-12-17 18:55:59 +0900

    음료차트 데이터 삽입 성공

---
+++ StudyCafe.Admin/BeverageChartActualForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace StudyCafe.Admin
+{
+    partial class BeverageChartActualForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram2 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.machineLearningTempBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.machineLearningTempBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.DataSource = this.machineLearningTempBindingSource;
+            xyDiagram2.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram2.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram2;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series2.Name = "Series 1";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series2};
+            this.chartControl1.SeriesTemplate.ArgumentDataMember = "DayQuater";
+            this.chartControl1.SeriesTemplate.ValueDataMembersSerializable = "UserCount";
+            this.chartControl1.Size = new System.Drawing.Size(800, 450);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // machineLearningTempBindingSource
+            // 
+            this.machineLearningTempBindingSource.DataSource = typeof(StudyCafe.Data.MachineLearningTemp);
+            // 
+            // BeverageChartActualForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "BeverageChartActualForm";
+            this.Text = "BeverageChartForm";
+            this.Load += new System.EventHandler(this.BeverageChartActualForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.machineLearningTempBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.BindingSource machineLearningTempBindingSource;
+    }
+}
--- StudyCafe.Data/Dao/UserDao.cs
+++ StudyCafe.Data/Dao/UserDao.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using EFLibrary;
+using StudyCafe.Data.Models;
 #endregion
 
 namespace StudyCafe.Data
@@ -29,9 +30,9 @@ namespace StudyCafe.Data
 
         public bool Exists(string text)
         {
-            using (var contexrt = new KoreanStudyCafeEntities())
+            using (var context = new KoreanStudyCafeEntities())
             {
-                return contexrt.Users.Any(x => x.PhoneNumber == text);
+                return context.Users.Any(x => x.PhoneNumber == text);
             }
         }
 
@@ -46,6 +47,90 @@ namespace StudyCafe.Data
                 return query.FirstOrDefault();
             }
         }
-  
+
+
+        public static List<StockControlModels> GetCoffeModel()
+        {
+
+            using (KoreanStudyCafeEntities context = new KoreanStudyCafeEntities())
+            {
+                var query = from x in context.BeverageRecord
+                            select new
+                            {
+                                UserCount = x.UserCount,
+                                DayQuarter = x.DayQuarter,
+                                Usage = x.Usage,
+                                BeverageId = x.BeverageID,
+                               
+                            };
+
+                var list = query.ToList();
+
+                List<StockControlModels> models = new List<StockControlModels>();
+                List<StockControlModels> beverages = new List<StockControlModels>();
+               
+                
+                foreach (var item in list)
+                {
+                   // StockControlModels beverage = beverages.Find(x => x.BeverageId == 1);
+                    StockControlModels model = models.Find(x => x.BeverageId == 1 && x.UserCount == item.UserCount && x.Usage == item.Usage);
+                        
+                    if (model != null)
+                    {
+                       // model.Quantity += item.Quantity;
+                    }
+                    else
+                    {
+                        model = new StockControlModels();
+                        
+                        model.UserCount = item.UserCount;
+                        model.Usage = item.Usage;
+                        model.DayQuater = item.DayQuarter;
+                      
+                        models.Add(model);
+
+                    }
+                }
+
+                return models;
+            }
+        }
+
+        public static List<StockControlModels> GetCocoaModel()
+        {
+            using(KoreanStudyCafeEntities context = new KoreanStudyCafeEntities())
+            {
+                var quary = from x in context.BeverageRecord
+                            select new
+                            {
+                                UserCount = x.UserCount,
+                                Useage = x.Usage,
+                                BeverageId = x.BeverageID,
+                                DayQuarter = x.DayQuarter
+                            };
+
+                var list = quary.ToList();
+
+                List<StockControlModels> models = new List<StockControlModels>();
+
+                foreach (var item in list)
+                {
+                    StockControlModels model = models.Find(x => x.BeverageId == 2 && x.Usage == item.Useage && x.UserCount == item.UserCount);
+
+                    if(model == null)
+                    {
+                        model = new StockControlModels();
+                        model.Usage = item.Useage;
+                        model.UserCount = item.UserCount;
+                        model.DayQuater = item.DayQuarter;
+
+                        models.Add(model);
+                    }
+
+                 
+                }
+                return models;
+            }
+        }
     }
 }
