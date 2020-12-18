b7f1cbcbb701d9bbe93dd2693521b9ade698037b
DAE-GYUN <f45ee@naver.com>
2020-12-18 17:30:35 +0900

    LogForm 생성

--- StudyCafe.Data/Dao/LogDao.cs
+++ StudyCafe.Data/Dao/LogDao.cs
@@ -1,13 +1,21 @@
 
 #region using
+using EFLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 #endregion
 
 namespace StudyCafe.Data
 {
-    public partial class LogDao
+    public partial class LogDao : SingleKeyDao<Log, int>
     {
-        
+        protected override Expression<Func<Log, int>> KeySelector => x => x.LogID;
+
+        protected override Expression<Func<Log, bool>> IsKey(int key)
+        {
+            return x => x.LogID == key;
+        }
     }
 }
